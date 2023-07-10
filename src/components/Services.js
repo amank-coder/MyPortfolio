42500d2b00eb87d2e7b04a8168a6c0242299d499
Aman Konchgamey <amanmaxwel@gmail.com>
2023-07-10 13:42:08 +0530

    added tech icons

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {SiSolidity, SiExpress} from 'react-icons/si'
+import {BiLogoJavascript, BiLogoPython, BiLogoCss3, BiLogoReact, BiLogoTailwindCss, BiLogoNodejs, BiLogoMongodb, BiLogoHtml5} from 'react-icons/bi'
+import {TbBrandCpp} from 'react-icons/tb' 
 
 const services =[
     {
@@ -16,7 +19,7 @@ const services =[
   ]
 const Services = () => {
   return(
-    <div className='section' id='service'>
+    <div className='section flex flex-col' id='service'>
       <div className='container justify-center flex-col lg:flex'>
         <div className='mx-auto'>
           <div className='p-12'>
@@ -39,6 +42,19 @@ const Services = () => {
               <p className='font-secondary text-center text-3xl mt-4 text-purple-300 font-semibold'>Contributor</p>
               <h4 className='text-[20px] tracking-wider font-primary font-semibold mb-6 mt-6 text-center'>TSOC '23</h4>
         </div>
+      </div>
+      <div className='mt-16 flex gap-8'>
+          <span><BiLogoHtml5 size={36}/> </span>
+          <span><BiLogoCss3 size={36}/></span>
+          <span><BiLogoTailwindCss size={36}/></span>
+          <span><BiLogoJavascript size={36}/></span>
+          <span><BiLogoReact size={36}/></span>
+          <span><BiLogoNodejs size={36}/></span>
+          <span><SiExpress size={36}/></span>
+          <span><BiLogoMongodb size={36}/></span>
+          <span><SiSolidity size={36}/></span>
+          <span><BiLogoPython size={36}/></span>
+          <span><TbBrandCpp size={36}/></span>
       </div>
     </div>
   ) 
